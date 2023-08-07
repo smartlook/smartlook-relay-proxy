7d2f195b1755be30794d73bb8092b9c78c4fa3aa
Sami Al-Dury <git@aldury.com>
2023-08-08 00:26:59 +0200

    fix: fastify typings

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,12 +1,26 @@
 import { fastifyHttpProxy } from '@fastify/http-proxy'
-import { fastify, type FastifyInstance, type FastifyRequest } from 'fastify'
+import {
+    fastify,
+    type FastifyInstance,
+    type FastifyRequest,
+    type RawReplyDefaultExpression,
+    type RawRequestDefaultExpression,
+    type RawServerDefault,
+} from 'fastify'
 
 import { config } from './config.js'
 import { rewriteRequestHeaders } from './http/headers.js'
 import { logger } from './logger.js'
 import { getRouteMappings } from './route-mappings.js'
 
-export async function bootstrap(): Promise<FastifyInstance> {
+export async function bootstrap(): Promise<
+    FastifyInstance<
+        RawServerDefault,
+        RawRequestDefaultExpression,
+        RawReplyDefaultExpression,
+        typeof logger
+    >
+> {
     const server = fastify({
         logger,
         trustProxy: config.trustProxy,
